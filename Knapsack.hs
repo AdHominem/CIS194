de7fe248bbd1eeeaaa8410d111974d7f0dce7615
jk <vargsmane@gmx.de>
2017-05-15 21:36:58 +0200

    More docs and added support for min and max ranges

--- Knapsack.hs
+++ Knapsack.hs
@@ -25,15 +25,17 @@ instance Show Item where
 getIndex :: Item -> Double
 getIndex item = fromIntegral (value item) / fromIntegral (weight item)
 
--- | Generates a list of random numbers which can later be used to create random Items
-getRandomNumbers :: (Random a, RandomGen g, Num a) => g -> Int -> [a]
-getRandomNumbers gen n = take n (randomRs (1, 10) gen)
-
--- | Turns a list of integers into a list of Items which has half the size, since every Item consumes two integers
-generateItems :: [Int] -> [Item]
-generateItems [] = []
-generateItems [x] = []
-generateItems (x:y:xs) = (Item x y) : generateItems xs
+{-|
+    Generates a list of random Items in a given range
+    The generator is changed with each iteration and the newly seeded one is passed to the next
+-}
+generateItems :: (RandomGen g) => g -> Int -> (Int, Int) -> [Item]
+generateItems generator n range
+    | n == 0 = []
+    | otherwise = (Item (fst value) (fst weight)) : generateItems (snd weight) (n - 1) range
+    where
+        value = randomR range generator
+        weight = randomR range (snd value)
 
 -- | Reads a line from stdout and ensures it is a positive integer
 getLineInt :: IO Int
@@ -61,10 +63,11 @@ fillKnapsack (i:is) sack limit = if weight i <= limit - getWeight sack
 -- | Creates a message indicating the success of the program
 generateSuccessMessage :: [Item] -> Int -> String
 generateSuccessMessage [] limit = "Arr! Our theft is forfeit! Next time we better take a bigger sack with us!"
-generateSuccessMessage sack limit = if limit == getWeight sack
+generateSuccessMessage sack limit = if limit == weight
     then "Awesome, we filled the whole bag! " ++ base
-    else "Nice! " ++ base
+    else "Nice! We used " ++ show weight ++ "/" ++ show limit ++ "kg!" ++ base
     where
+        weight = getWeight sack
         base = "We got " ++ show (length sack) ++ " items totaling " ++ show (sum (map value sack)) ++ "€!"
 
 main :: IO ()
@@ -74,9 +77,15 @@ main = do
     putStrLn "Please enter the amount of items to generate: "
     itemCount <- getLineInt
 
+    putStrLn "Please enter the minimum number for weights and values: "
+    minNum <- getLineInt
+
+    putStrLn "Please enter the maximum number for weights and values: "
+    maxNum <- getLineInt
+
     putStrLn $ "\nGenerating " ++ (show itemCount) ++ " items:"
     gen <- getStdGen
-    let items = sort (generateItems (getRandomNumbers gen (itemCount * 2)))
+    let items = sort (generateItems gen itemCount (minNum, maxNum))
     mapM_ print items
 
     putStrLn "\nPlease enter the maximum weight: "
@@ -89,6 +98,3 @@ main = do
     let filledSack = fillKnapsack items knapsack weightLimit
     mapM_ print filledSack
     putStrLn $ generateSuccessMessage filledSack weightLimit
-
-
-
